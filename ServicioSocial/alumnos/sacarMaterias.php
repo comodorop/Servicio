5d512d31607114b8765b376b621406f79e5dad2e
Comodoro <Comodoro@pjgt>
2013-12-04 17:12:13 -0600

    ass

--- ServicioSocial/alumnos/sacarMaterias.php
+++ ServicioSocial/alumnos/sacarMaterias.php
@@ -3,15 +3,11 @@ session_start();
 include '../DaoConnection/coneccion.php';
 $cn = new coneccion();
 $alumno = $_SESSION['UsuarioAlumno'];
-$fecha=$_SESSION["cicloEscolar"];
-$sql = "SELECT cicloescolar, anio FROM fechascicloescolar f, curso c WHERE f.cicloEscolar='$fecha' AND c.id=f.cicloEscolar AND f.idControl=0";
-$datos = mysql_query($sql, $cn->Conectarse());
-while ($rs = mysql_fetch_array($datos)) {
-    $cicloescolar = $rs[0];
-    $anio = $rs[1];
-}
-
-$sql2 = "SELECT DISTINCT idMateria,materia FROM calificacionesactual c, materias m WHERE c.CursoEscolar='$cicloescolar' AND c.anio='$anio' AND c.idAlumno='$alumno' AND c.idMateria=m.id";
+$sql2 = "SELECT * FROM alumnosinscritos ai, materias m
+where usuario = '$alumno'
+and ai.idMateria = m.id
+and anio = '".$_SESSION["anio"]."'
+and cursoEscolar = '".$_SESSION["cicloEscolar"]."'";
 $datos2 = mysql_query($sql2, $cn->Conectarse());
 if ($datos2 == "") {
 //    echo '<select id="materias" name="materia"  >
