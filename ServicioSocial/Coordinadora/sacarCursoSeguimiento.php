e0a508ab36b85b512ab3e419eba4cf606c28311b
Angel Solis <Angel Solis@AngelSolis-PC>
2014-01-11 19:15:29 -0600

    encuesta segumiento curso estadisticos

---
+++ ServicioSocial/Coordinadora/sacarCursoSeguimiento.php
@@ -0,0 +1,21 @@
+
+<?php
+include '../DaoConnection/coneccion.php';
+$cn = new coneccion();
+$sql = "SELECT id, curso FROM curso";
+
+$datos = mysql_query($sql, $cn->Conectarse());
+if ($datos == "") {
+    echo '<select id="materia" name="materia"  >
+          <option>Seleccione el maestro</option>
+          </select>';
+} else {
+    echo "<select name='materia' id='materia' >";
+    echo"<option>Seleccione el maestro</option>";
+    While ($rs = mysql_fetch_array($datos)) {
+        ?>
+        <option value="<?php echo $rs["id"]; ?>"><?php echo $rs["curso"] ?></option>
+        <?php
+    }
+    echo "</select>";
+}
---
+++ ServicioSocial/Coordinadora/selectAnioSeguimiento.php
@@ -0,0 +1,22 @@
+<?php
+include '../DaoConnection/coneccion.php';
+$cn = new coneccion();
+$sql = "SELECT anio FROM seguimientocurso s \n"
+    . "inner join maestros m\n"
+    . "on s.matriculaMaestro = m.id ";
+
+$datos = mysql_query($sql, $cn->Conectarse());
+if ($datos == "") {
+    echo '<select id="materia" name="materia"  >
+          <option>Seleccione el maestro</option>
+          </select>';
+} else {
+    echo "<select name='materia' id='materia' >";
+    echo"<option>Seleccione el maestro</option>";
+    While ($rs = mysql_fetch_array($datos)) {
+        ?>
+        <option value="<?php echo $rs["anio"]; ?>"><?php echo $rs["anio"] ?></option>
+        <?php
+    }
+    echo "</select>";
+}
