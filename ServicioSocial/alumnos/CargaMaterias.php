676ffd0c79b2778751ad9bff0ca4e173ba23382f
Angel Solis <Angel Solis@AngelSolis-PC>
2014-01-12 16:25:24 -0600

    precarga una vez mas

--- ServicioSocial/alumnos/CargaMaterias.php
+++ ServicioSocial/alumnos/CargaMaterias.php
@@ -1,4 +1,3 @@
-
 <?php
 //include '../DaoConnection/coneccion.php';
 session_start();
@@ -6,7 +5,7 @@ include './plantilla.php';
 include '../Dao/dao.php';
 include './validacionseSessionAlumnos.php';
 include '../clases/fechascicloescolar.php';
-include '../clases/verificacion.php';
+//include '../clases/verificacion.php';
 
 $dao = new Dao();
 $fechascicloescolar = new fechascicloescolar();
@@ -35,100 +34,105 @@ if ($valido != true) {
 
 
     if ($d == 0) {
-        ?>
-        <html>
-            <head>
-                <style>
-                    .stlconten{
-                        background-color: white;
-                        -webkit-background-size: cover; 
-                        -moz-background-size: cover; 
-                        -o-background-size: cover; 
-                        background-size: cover;
-                        margin-top: -20px
+?>
+<html>
+    <head>
+        <style>
+            .stlconten{
+                background-color: white;
+                -webkit-background-size: cover; 
+                -moz-background-size: cover; 
+                -o-background-size: cover; 
+                background-size: cover;
+                margin-top: -20px
+            }
+        </style>
+        <script>
+            $(document).ready(function() {
+
+
+
+                $('#bien').hide();
+                $('#mal').hide();
+                $('#malo').hide();
+                $('#botonazo').click(function() {
+
+                    var busqueda = $("#origen option").length;
+                    var valor = $('#horario').val();
+                    if (valor != 0) {
+
+                        if (busqueda <= 6 && busqueda > 4) {
+                            $('#origen option').prop('disabled', false);
+                            $('#origen option').prop('selected', 'selected');
+                            var datos = 'Asignatura=' + $('#origen').val() +
+                                    '&valor=' + valor;
+                            $.get('GuardarCarga.php', datos, function() {
+                                $('#bien').show('slow');
+                                $('#bien').hide('slow');
+                            })
+                        } else {
+                            $('#mal').show('slow');
+                            $('#mal').hide('slow');
+                        }
+
                     }
-                </style>
-                <script>
-                    $(document).ready(function() {
+                    else {
+                        $('#malo').show('slow');
+                        $('#malo').hide('slow');
+                    }
+                });
 
-
-
-
-                        $('#botonazo').click(function() {
-
-                            var busqueda = $("#origen option").length;
-                            var valor = $('#horario').val();
-                            if (valor != 0) {
-
-                                if (busqueda <= 6 && busqueda > 4) {
-                                    $('#origen option').prop('disabled', false);
-                                    $('#origen option').prop('selected', 'selected');
-                                    var datos = 'Asignatura=' + $('#origen').val() +
-                                            '&valor=' + valor;
-                                    $.get('GuardarCarga.php', datos, function(valor) {
-                                        var paso = $.parseJSON(valor);
-                                        if (valor != 0) {
-                                            alertify.success("Carga Exitosa");
-                                            location.href = 'index.php';
-                                        }
-                                        else {
-                                            alertify.alert("<b>Ya has cargado anteriormente", function() {
-                                                location.href = 'index.php';
-                                            });
-                                        }
-                                    })
-                                } else {
-                                    alertify.error("Debes seleccionar de 5 a 6 materias");
-                                }
-
-                            }
-                            else {
-                                alertify.error("Debes seleccionarun horario");
-                            }
-                        });
-
-                        $().ready(function()
-                        {
-                            $('.pasar').click(function() {
-                                return !$('#origen option:selected').remove().appendTo('#destino');
-                            });
-                            $('.quitar').click(function() {
-                                return !$('#destino option:selected').remove().appendTo('#origen');
-                            });
-                            $('.pasartodos').click(function() {
-                                $('#origen option').each(function() {
-                                    $(this).remove().appendTo('#destino');
-                                });
-                            });
-                            $('.quitartodos').click(function() {
-                                $('#destino option').each(function() {
-                                    $(this).remove().appendTo('#origen');
-                                });
-                            });
-                            $('.submit').click(function() {
-                                $('#destino option').prop('selected', 'selected');
-                            });
+                $().ready(function()
+                {
+                    $('.pasar').click(function() {
+                        return !$('#origen option:selected').remove().appendTo('#destino');
+                    });
+                    $('.quitar').click(function() {
+                        return !$('#destino option:selected').remove().appendTo('#origen');
+                    });
+                    $('.pasartodos').click(function() {
+                        $('#origen option').each(function() {
+                            $(this).remove().appendTo('#destino');
                         });
                     });
-                </script>
-            </head> 
-            <body>
-                <div class="container stlconten">
-                    <div class="span12" style="margin: auto"> 
-                        <div style="margin: 3%">
-                            <div class="well well-sm pagination-centered">
-                                <select id="horario" style="width: 250px">
-                                    <option value="0">En que horario?</option>
-                                    <option value="Matutino">Matutino</option>
-                                    <option value="Vespertino">Vespertino</option>
-                                    <
-                                </select> 
+                    $('.quitartodos').click(function() {
+                        $('#destino option').each(function() {
+                            $(this).remove().appendTo('#origen');
+                        });
+                    });
+                    $('.submit').click(function() {
+                        $('#destino option').prop('selected', 'selected');
+                    });
+                });
+            });
+        </script>
+    </head> 
+    <body>
+        <div class="container stlconten">
+            <div class="span12" style="margin: auto"> 
+                <div style="margin: 3%">
+                    <div class="well well-sm pagination-centered">
+                        <select id="horario" style="width: 250px">
+                            <option value="0">En que horario?</option>
+                            <option value="Matutino">Matutino</option>
+                            <option value="Vespertino">Vespertino</option>
+                            <
+                        </select> 
+                        <div id="bien" class="alert alert-success">
+                            <strong>Se han Guardado las materias satisfactoriamente</strong>
+                        </div>
+                        <div id="mal" class="alert alert-error">
+                            <strong> Deben ser 5 ・6 materias seleccionadas</strong>
+                        </div>
 
-                                <div class="row">
-                                    <select name="destino[]" id="destino" multiple="multiple" size="8" style="height: 30%; width: 30%">
-                                        <?php
+                        <div id="malo" class="alert alert-error">
+                            <strong> Debes seleccionar el turno </strong>
+                        </div>
+                        <div class="row">
+                            <select name="destino[]" id="destino" multiple="multiple" size="8" style="height: 30%; width: 30%">
+                             <?php
                                         $cn = new coneccion();
-                                        $sql = "SELECT distinct concat_ws('-_- ', m.semestre, m.materia) as fusion, m.materia  FROM materias m,historial h WHERE idAcreditacion <=2 and h.calificacion > 70 and m.id NOT IN (SELECT idMateria FROM historial where usuario='$matricula' )LIMIT 0 , 10";
+                                        $sql = "SELECT distinct concat_ws('-_- ', m.semestre, m.materia) as fusion, m.materia  FROM materias m,historial h WHERE idAcreditacion <=2 and h.calificacion > 70 and m.id NOT IN (SELECT idMateria FROM historial where usuario='$matricula' )LIMIT 0 , 20";
                                         $datos = mysql_query($sql, $cn->Conectarse());
 
                                         if ($sql != false) {
@@ -156,7 +160,12 @@ if ($valido != true) {
 
                                                 if ($campo == "materia") {
 
-
+ if (utf8_encode($valor) === "Taller de ética" || utf8_encode($valor) === "Dibujo Industrial" || utf8_encode($valor) === "Electricidad y Electrónica Industrial" || utf8_encode($valor) === "Ingeniería de Sistemas" || utf8_encode($valor) === "Análisis de la Realidad Nacional" || utf8_encode($valor) === "Taller de Liderazgo" || utf8_encode($valor) === "Metrología y Normalización" || utf8_encode($valor) === "Higiene y Seguridad Industrial" || utf8_encode($valor) === "Administración de Proyectos" || utf8_encode($valor) === "Desarrollo Sustentable" || utf8_encode($valor) === "Administración del Mantenimiento" || utf8_encode($valor) === "Sistemas de Manufactura"|| utf8_encode($valor) === "Logística y Cadenas de Suministro"|| utf8_encode($valor) === "Gestión de los Sistemas de Calidad"|| utf8_encode($valor) === "Prospectiva Tecnológica"|| utf8_encode($valor) === "Almacenimiento"|| utf8_encode($valor) === "Relaciones Industriales"|| utf8_encode($valor) === "Aprovisionamiento"|| utf8_encode($valor) === "Logística de Fabricación"|| utf8_encode($valor) === "Diseño Industrial"|| utf8_encode($valor) === "Manufactura Esbelta" || utf8_encode($valor) === "Residencia Profesional" || utf8_encode($valor) === "Servicio Social" || utf8_encode($valor) === "Actividades Complementarias" || utf8_encode($valor) === "Seminario de Logística Internacional" || utf8_encode($valor) === "Transportación y Distribución" || utf8_encode($valor) === "Simulación de Sistemas Lógicos y Productivos" || utf8_encode($valor) === "Simulación y Optimización de Procesos de Manufactura" || utf8_encode($valor) === "Atomatización Industrial"|| utf8_encode($valor) === "Medicion y Mejoramiento de la Productividad"){
+              $materia = $valor;
+                                                        
+                                                    } 
+                                                    
+                                                    
                                                     if (utf8_encode($valor) == "Cálculo Diferencial") {
                                                         $materia = $valor;
                                                         $control = 1;
@@ -314,7 +323,7 @@ if ($valido != true) {
                                                 }
                                                 if (($materia && $fusion) != "") {
                                                     ?>
-                                                    <option value="<?php echo utf8_encode($materia); ?>" disabled="true"><?php echo utf8_encode($fusion) ?></option>
+                                                    <option value="<?php echo utf8_encode($materia); ?>" ><?php echo utf8_encode($fusion) ?></option>
                                                     <?php
                                                     $fusion = "";
                                                     $materia = "";
@@ -323,64 +332,55 @@ if ($valido != true) {
                                             }
                                         }
                                         ?>
-                                    </select>
-                                    <select name="origen[]" id="origen" multiple="multiple" size="8" style="height: 30%; width: 30%">
-                                        <?php
-                                        $cn = new coneccion();
-                                        $sql = "SELECT concat_ws(' ', materias.semestre, materias.materia) as fusion, max(calificacion) as valor, max(idAcreditacion) as acredita,max(ingresoCursado) FROM materias, historial where historial.idMateria = materias.id and usuario = '$matricula' group by historial.idMateria";
-                                        $datos2 = mysql_query($sql, $cn->Conectarse());
-                                        While ($rs2 = mysql_fetch_array($datos2)) {
-                                            if ($rs2["valor"] < 70) {
-                                                $valor = 1;
-                                                if ($rs2["acredita"] < 2 && $valor == 1) {
-                                                    ?>
-                                                    <option value="<?php echo utf8_encode($rs2["materia"]); ?>" disabled="true"><?php echo utf8_encode($rs2["fusion"]) ?></option>
-                                                    <?php
-                                                    $valor = 0;
-                                                } else {
-                                                    $valor = 0;
-                                                }
-                                            } else {
-                                                $valor = 0;
-                                            }
-                                        }
-                                        ?>
-                                    </select>
-                                </div>
-                                <input type="button" class="quitar der btn btn-success" value="Agregar a la lista">
-                                <input type="button" class="pasar izq btn btn-danger" value="Quitar de la lista">
-                            </div>
-                            <?php
-                            echo $ciclo;
-                            $cn = new coneccion();
-                            $sql = " SELECT p.idMateria, count(p.idMateria) as prr, m.materia \n"
-                                    . "FROM precarga p, materias m \n"
-                                    . "where p.idMateria = m.id\n"
-                                    . "group by p.idMateria \n"
-                                    . " LIMIT 0, 30 ";
-                            $datos = mysql_query($sql, $cn->Conectarse());
-                            ?><table BORDER=1>
-                                <th> materia </th><th> Alumnos Preinscritos</th>
+                                
+                                
+                                
+                                
+                            </select>
+                            <select name="origen[]" id="origen" multiple="multiple" size="8" style="height: 30%; width: 30%">
                                 <?php
-                                While ($rs = mysql_fetch_array($datos)) {
+                                $cn = new coneccion();
+                                $sql = "SELECT concat_ws('-_- ', m.semestre, m.materia) as fusion, m.materia, m.semestre, m.id FROM historial h, materias m where h.usuario = '$matricula' and h.idAcreditacion <=2 and h.calificacion < 70 and m.id = h.idMateria ";
+
+                                $datos2 = mysql_query($sql, $cn->Conectarse());
+                                While ($rs2 = mysql_fetch_array($datos2)) {
                                     ?>
-                                    <tr> <td><?php echo utf8_encode($rs["materia"]); ?></td> <td><?php echo utf8_encode($rs["prr"]); ?></td> 
-                                        <?php
-                                    }
-                                    ?></tr>
-                            </table>
-                            <?php
-                            $cn->cerrarBd();
-                            ?>
+                                <option value="<?php echo utf8_encode($rs2["materia"]); ?>" disabled="true"><?php echo utf8_encode($rs2["fusion"]) ?></option>
+                                    <?php
+                                }
+                                ?>
+                            </select>
                         </div>
-                        <input id="botonazo" class="btn btn-primary" type="button" title="" value="Guardar materias" >
+                        <input type="button" class="quitar der btn btn-success" value="Agregar a la lista">
+                        <input type="button" class="pasar izq btn btn-danger" value="Quitar de la lista">
                     </div>
+                      <?php
+           
+            $cn = new coneccion();
+           $sql = "SELECT m.materia, count(p.idMateria) as prr FROM precarga p JOIN historial h ON h.usuario = p.Matricula join materias m on p.idMateria = m.id WHERE p.Matricula = '$matricula' and p.idMateria NOT IN (SELECT h.idMateria FROM historial h where h.usuario='$matricula')GROUP by p.idMateria";
+            $datos = mysql_query($sql, $cn->Conectarse());
+            ?><table BORDER=1>
+                <th> materia </th><th> Alumnos Preinscritos</th>
+                <?php
+            While ($rs = mysql_fetch_array($datos)) {
+                ?>
+                <tr> <td><?php echo utf8_encode($rs["materia"]);?></td> <td><?php echo utf8_encode($rs["prr"]);?></td> 
+            <?php
+            
+            }
+             ?></tr>
+            </table>
+            <?php
+            $cn->cerrarBd();
+            ?>
+                </div>
+                    <input id="botonazo" class="btn btn-primary" type="button" title="" value="Guardar materias" >
                 </div>
             </div>
-        </body>
-        </html>
-
-        <?php
+        </div>
+    </body>
+</html>
+ <?php
         include './plantillaFooter.php';
     } else {
         echo "<script>alertify.alert('<b>Ya has cargado anteriormente', function() {
--- ServicioSocial/clases/verificacion.php
+++ ServicioSocial/clases/verificacion.php
@@ -21,69 +21,71 @@ class verificacion {
     private $ano;
     private $usuarioMaestro;
 
-    public function getAno() {
-        return $this->ano;
-    }
-
-    public function setAno($ano) {
-        $this->ano = $ano;
-    }
-
     public function getId() {
         return $this->id;
-    }
-
-    public function setId($id) {
-        $this->id = $id;
     }
 
     public function getMatricula() {
         return $this->matricula;
     }
 
-    public function setMatricula($matricula) {
-        $this->matricula = $matricula;
-    }
-
     public function getControl() {
         return $this->control;
-    }
-
-    public function setControl($control) {
-        $this->control = $control;
     }
 
     public function getTipo() {
         return $this->tipo;
     }
 
-    public function setTipo($tipo) {
-        $this->tipo = $tipo;
-    }
-
     public function getCiclo() {
         return $this->ciclo;
-    }
-
-    public function setCiclo($ciclo) {
-        $this->ciclo = $ciclo;
     }
 
     public function getAño() {
         return $this->año;
     }
 
-    public function setAño($año) {
-        $this->año = $año;
+    public function getAno() {
+        return $this->ano;
     }
 
     public function getUsuarioMaestro() {
         return $this->usuarioMaestro;
     }
 
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setMatricula($matricula) {
+        $this->matricula = $matricula;
+    }
+
+    public function setControl($control) {
+        $this->control = $control;
+    }
+
+    public function setTipo($tipo) {
+        $this->tipo = $tipo;
+    }
+
+    public function setCiclo($ciclo) {
+        $this->ciclo = $ciclo;
+    }
+
+    public function setAño($año) {
+        $this->año = $año;
+    }
+
+    public function setAno($ano) {
+        $this->ano = $ano;
+    }
+
     public function setUsuarioMaestro($usuarioMaestro) {
         $this->usuarioMaestro = $usuarioMaestro;
     }
+
+
 
 }
 
